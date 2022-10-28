788c73725771de76006e30d1dc3704a506ca95e3
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-10-27 22:32:26 -0300

    ajustado descrição

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,9 +3,11 @@
 const AboutContainer = () => {
   return <section className="about-container">
     <h2>Sobre</h2>
-    <p>Sou formando em Engenharia de Controle e Automação, atualmente estou cursando Análise e Desenvolvimento de Sistemas. Possuo conhecimentos básico de Git, Terminal, Lógica, Algoritmo, Estrutura de dados, HTML,CSS, Javascript, ReactJs, NodeJs, Excel e VBA.
-Atualmente sou estagiário de desenvolvimento Front-end, desenvolvendo aplicações web, utilizando React Js e NodeJs, consumo de API REST.</p>
+    <p>Sou formando em Engenharia de Controle e Automação, atualmente estou cursando Análise e Desenvolvimento de Sistemas. Possuo conhecimentos básico de Git, Terminal, Lógica, Algoritmo, Estrutura de dados, HTML,CSS, Javascript, ReactJs, NodeJs, Excel e VBA.</p>
+    <p>Atualmente sou estagiário de desenvolvimento Front-end, desenvolvendo aplicações web, utilizando React Js e NodeJs, consumo de API REST.</p>
+    <p>Tive uma experiência anterior no segmento de BI, desenvolvendo indicadores de performance, criação e manutenção de relatórios, placares e dashboards em excel, no segmento de cotabilidade</p>
     <p>Meu próximo passo será aprender Typescript e React-Native</p>
+     
   </section>
 }
 
--- src/components/TechContainer.jsx
+++ src/components/TechContainer.jsx
@@ -7,8 +7,9 @@ const technologies = [
   {id:'css', name:'CSS 3', icon:<SiCss3/>, desc:'Conhecimento básico', site:'https://developer.mozilla.org/pt-BR/docs/Web/Css'},
   {id:'js', name:'Javascript', icon:<SiJavascript/>, desc:'Conhecimento básico', site:'https://developer.mozilla.org/pt-BR/docs/Web/javascript'},
   {id:'node', name:'Node.js', icon:<SiNodedotjs/>, desc:'Conhecimento básico', site:'https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs/Introduction'},
-  {id:'type', name:'Typescript', icon:<SiTypescript/>, desc:'Conhecimento básico', site:'https://developer.mozilla.org/pt-BR/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_TypeScript'},
+  {id:'type', name:'Typescript', icon:<SiTypescript/>, desc:'Em desenvolvimento', site:'https://developer.mozilla.org/pt-BR/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_TypeScript'},
   {id:'react', name:'React', icon:<SiReact/>, desc:'Conhecimento básico',site:'https://developer.mozilla.org/pt-BR/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started'},
+  {id:'reactnative', name:'React Native',icon:<SiReact/>, desc:'Em desenvolvimento', site:'https://reactnative.dev/'},
   {id:'excel', name:'Excel', icon:<SiMicrosoftexcel/>, desc:'Conhecimento básico',site:'https://www.microsoft.com/pt-br/microsoft-365/p/excel-home-and-student/cfq7ttc0hlkr'},
 ]
 
